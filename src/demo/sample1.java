ce44c19f10548f494a99e189918a5421175863ab
Admin123 <Admin123@DESKTOP-K5U87LI>
2022-07-20 10:52:55 +0530

    [yogesh]:new added5

---
+++ src/demo/sample1.java
@@ -0,0 +1,29 @@
+package demo;
+
+public class sample1
+{
+	public static void main(String[] args) 
+	{
+		
+		
+		
+		System.out.println("hello");
+		
+		System.out.println("i am yogesh");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
